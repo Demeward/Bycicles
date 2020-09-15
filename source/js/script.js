72d1a4edb24e657ae65517dfbdf96696f23da7e8
Demeward <did.dod1520@mail.ru>
2020-09-15 11:36:57 +0300

    Плавный скролл по ссылкам-якорям

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,35 @@
 'use strict';
-var menuButton = document.querySelector('.main-nav__toggle');
-var menu = document.querySelector('.main-nav');
 
-menu.classList.remove('main-nav--no-js');
-menuButton.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  menu.classList.toggle('main-nav--closed');
-});
+(function () {
+  var menuButton = document.querySelector('.main-nav__toggle');
+  var menu = document.querySelector('.main-nav');
+
+  menu.classList.remove('main-nav--no-js');
+  menuButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    menu.classList.toggle('main-nav--closed');
+  });
+
+  var getToAnchor = function (event, anchor) {
+    event.preventDefault();
+
+    var blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  };
+
+  var anchors = document.querySelectorAll('a[href*="#"]');
+
+
+  for (var i = 0; i < anchors.length; i++) {
+    (function () {
+      var anchor = anchors[i];
+      anchor.addEventListener('click', function () {
+        getToAnchor(event, anchor);
+      });
+    })();
+  }
+})();

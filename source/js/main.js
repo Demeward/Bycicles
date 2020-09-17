2d42a02e50e06dbcc04e7af55d25044f9a5aa853
Demeward <did.dod1520@mail.ru>
2020-09-17 14:42:33 +0300

    Исправления багов

--- source/js/main.js
+++ source/js/main.js
@@ -35,4 +35,50 @@
       });
     })();
   }
+
+  window.addEventListener('DOMContentLoaded', function () {
+    function setCursorPosition(pos, elem) {
+      elem.focus();
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+      }
+    }
+
+    function mask() {
+      var matrix = '+7 (___) ___-__-__';
+      var m = 0;
+      var def = matrix.replace(/\D/g, '');
+      var val = input.value.replace(/\D/g, '');
+      if (def.length >= val.length) {
+        val = def;
+      }
+      input.value = matrix.replace(/./g, function (a) {
+
+        if (/[_\d]/.test(a) && m < val.length) {
+          return val.charAt(m++);
+        } else if (m >= val.length) {
+          return '';
+        } else {
+          return a;
+        }
+      });
+      if (event.type === 'blur') {
+        if (input.value.length === 2) {
+          input.value = '';
+        }
+      } else {
+        setCursorPosition(input.value.length, input);
+      }
+    }
+    var input = document.querySelector('[name="tel"]');
+    input.addEventListener('input', mask, false);
+    input.addEventListener('focus', mask, false);
+    input.addEventListener('blur', mask, false);
+  });
 })();
